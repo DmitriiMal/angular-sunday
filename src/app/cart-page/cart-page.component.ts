9c5046516529a4f4ca0d6edcc9b7cd5b47696ddc
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2023-10-22 22:52:55 +0200

    add getCart, calcSum, setDiscount, calcTotal

--- src/app/cart-page/cart-page.component.ts
+++ src/app/cart-page/cart-page.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Iproducts } from '../Iproducts';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart-page',
   templateUrl: './cart-page.component.html',
   styleUrls: ['./cart-page.component.scss'],
 })
-export class CartPageComponent {}
+export class CartPageComponent implements OnInit {
+  cart: Iproducts[] = [];
+  sum: number = 0;
+  service: number = 3.1;
+  discount: number = 0;
+  total: number = 0;
+
+  constructor(private CS: CartService) {}
+  ngOnInit(): void {
+    this.cart = this.CS.getCart();
+    this.sum = +this.CS.calcSum().toFixed(2);
+    this.discount = this.CS.setDiscount(this.sum, this.discount);
+    this.total = +this.CS.calcTotal(
+      this.sum,
+      this.discount,
+      this.service
+    ).toFixed(2);
+  }
+}
--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -6,11 +6,49 @@ import { Iproducts } from './Iproducts';
 })
 export class CartService {
   cart: Iproducts[] = [];
+  // discount: number = 0;
+  // total: number = 0;
+
   constructor() {}
 
   addToCart(obj: Iproducts) {
     this.cart.push(obj);
     console.table(this.cart);
     alert('Product addet to the cart');
+  }
+
+  getCart() {
+    return this.cart;
+  }
+
+  calcSum() {
+    let sum: number = 0;
+
+    this.cart.forEach((prod) => {
+      sum += prod.price;
+    });
+    return sum;
+  }
+
+  setDiscount(sum: number, discount: number) {
+    if (sum > 20) {
+      discount = 15;
+    } else {
+      discount = 0;
+    }
+
+    return discount;
+  }
+
+  calcTotal(sum: number, discount: number, service: number) {
+    let total: number = 0;
+    if (sum == 0) {
+      total = 0;
+    } else if (discount == 0) {
+      total = sum + service;
+    } else {
+      total = sum - (sum * discount) / 100 + service;
+    }
+    return total;
   }
 }
