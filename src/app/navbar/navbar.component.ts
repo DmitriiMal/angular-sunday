bf96bbe01e55c59a88d9e1ee6b8a59036ed2e51c
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2023-10-22 23:47:24 +0200

    add Badge to the cart link

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { Iproducts } from '../Iproducts';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  cart: Iproducts[] = [];
 
+  constructor(private CS: CartService) {}
+
+  ngOnInit(): void {
+    this.cart = this.CS.getCart();
+  }
 }
